4d589960195a893fdd4a856ea5165aa156eff0f0
Андрей Тарасенко <mikatohimemiya@gmail.com>
2023-10-11 00:17:08 +0300

    Add project files.

---
+++ Models/TelegramMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlanTelegramBotApp.Models
+{
+    public class TelegramMessage
+    {
+        public int MessageId { get; set; }
+        public required string Content { get; set; }
+        public long TelegramId { get; set; }
+    }
+}
